3626186aee319221f3cfdaf63290343c7fd4da25
zotyag <62486626+zotyag@users.noreply.github.com>
2025-04-13 01:05:28 +0200

    Update kibov_eukli.py

--- Uni/kibov_eukli.py
+++ Uni/kibov_eukli.py
@@ -1,12 +1,26 @@
 def kibovitett_euklidesz(a, b):
-    regi_r, r = a, b
-    regi_s, s = 1, 0
-    regi_t, t = 0, 1
-
-    while r != 0:
-        hanyados = regi_r // r
-        regi_r, r = r, regi_r - hanyados * r
-        regi_s, s = s, regi_s - hanyados * s
-        regi_t, t = t, regi_t - hanyados * t
-
-    return (regi_r, regi_s, regi_t)
+    x0, x1 = 1, 0
+    y0, y1 = 0, 1
+    s = 1
+    
+    while b != 0:
+        r = a % b
+        q = a // b
+        
+        a, b = b, r
+        
+        x, y = x1, y1
+        
+        x1 = q * x1 + x0
+        y1 = q * y1 + y0
+        
+        x0, y0 = x, y
+        
+        s = -s
+    
+    x = s * x0
+    y = -s * y0
+    
+    d = a
+    
+    return (d, x, y)
